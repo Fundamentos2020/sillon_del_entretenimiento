3e9e4bc42b7986734d9f396a155601ce2e13c037
RammasEchor <35913846+RammasEchor@users.noreply.github.com>
2020-04-14 15:23:59 -0500

    Scripts para cada página

---
+++ index.js
@@ -0,0 +1,22 @@
+const menu_noticias = document.querySelector('#menu_noticias');
+const cerrar_menu_movil = document.querySelector('#cerrar_menu_movil');
+const boton_menu_movil = document.querySelector('#boton_menu');
+
+menu_noticias.addEventListener('click', loadNoticias );
+boton_menu_movil.addEventListener('click', mostrarMenu );
+cerrar_menu_movil.addEventListener('click', ocultarMenu );
+
+function loadNoticias()  {
+    
+}
+
+function mostrarMenu()  {
+    const menu = document.querySelector('#menu_movil');
+    menu.style.display = 'inline' ;
+    menu.style.width = '300px' ;
+}
+
+function ocultarMenu()  {
+    const menu = document.querySelector('#menu_movil');
+    menu.style.width = '0px';
+}
